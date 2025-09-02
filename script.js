3192941258aab4e0c2e61f1bf5687363189422fa
lucho <nicolasluciano1425@gmail.com>
2025-09-02 20:09:21 -0300

    Menu Nav

---
+++ script.js
@@ -0,0 +1,6 @@
+     function myFunction(x) {
+        x.classList.toggle("change");
+
+         const menu = document.getElementById("menu");
+        menu.style.display = (menu.style.display === "flex") ? "none" : "flex";
+      }
