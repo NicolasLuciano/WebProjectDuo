2b972442f1b76ef7b5bb4ba31920bc1f7adad57a
lucho <nicolasluciano1425@gmail.com>
2025-09-03 13:00:48 -0300

    Footer add

--- script.js
+++ script.js
@@ -4,3 +4,19 @@
          const menu = document.getElementById("menu");
         menu.style.display = (menu.style.display === "flex") ? "none" : "flex";
       }
+let lastScroll = 0;
+    const navbar = document.getElementById("navbar");
+
+    window.addEventListener("scroll", () => {
+      const currentScroll = window.pageYOffset;
+
+      if (currentScroll > lastScroll) {
+        // scrolleando hacia abajo
+        navbar.classList.add("hidden");
+      } else {
+        // scrolleando hacia arriba
+        navbar.classList.remove("hidden");
+      }
+
+      lastScroll = currentScroll;
+    });
